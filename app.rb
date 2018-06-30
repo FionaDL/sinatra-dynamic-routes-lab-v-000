6b975da81225383ee182d659af81781e3eb9fc37
Fiona Lapham <fionadlapham@gmail.com>
2018-06-30 10:29:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @phrase = params[:phrase]
     @number = params[:number].to_i
+    binding.pry
     "#{@number}".times do
       puts "#{@phrase}"
     end
