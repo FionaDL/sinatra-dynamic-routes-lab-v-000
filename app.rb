688d95a27015aeb7ca23ae6f2b19c95157f6b81a
Fiona Lapham <fionadlapham@gmail.com>
2018-06-30 10:36:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,10 +15,11 @@ class App < Sinatra::Base
   end
 
   get "/say/:number/:phrase" do
+    answer = " "
     @phrase = params[:phrase]
     @number = params[:number].to_i
-    @repeated_phrase = @number.times {puts @phrase}
-    @repeated_phrase
+     @number.times {@answer += puts @phrase}
+    @answer
   end
 
 
